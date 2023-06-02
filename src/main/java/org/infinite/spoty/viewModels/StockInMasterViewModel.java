c8e2f5a077e513faaa5ed8e07bf318f503db0c9e
infinite-dev22 <mwigojm@gmail.com>
2023-06-02 08:47:29 +0300

    Create StockInMasterViewModel

---
+++ src/main/java/org/infinite/spoty/viewModels/StockInMasterViewModel.java
@@ -0,0 +1,126 @@
+package org.infinite.spoty.viewModels;
+
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.infinite.spoty.database.dao.StockInMasterDao;
+import org.infinite.spoty.database.models.Branch;
+import org.infinite.spoty.database.models.StockInMaster;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StockInMasterViewModel {
+    public static final ObservableList<StockInMaster> stockInMasterList = FXCollections.observableArrayList();
+    private static final IntegerProperty id = new SimpleIntegerProperty(0);
+    private static final StringProperty date = new SimpleStringProperty("");
+    private static final ObjectProperty<Branch> branch = new SimpleObjectProperty<>(null);
+    private static final StringProperty totalCost = new SimpleStringProperty("");
+    private static final StringProperty status = new SimpleStringProperty("");
+    private static final StringProperty note = new SimpleStringProperty("");
+
+    public static int getId() {
+        return id.get();
+    }
+
+    public static void setId(int id) {
+        StockInMasterViewModel.id.set(id);
+    }
+
+    public static IntegerProperty idProperty() {
+        return id;
+    }
+
+    public static Date getDate() {
+        try {
+            return new SimpleDateFormat("MMM dd, yyyy").parse(date.get());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setDate(String date) {
+        StockInMasterViewModel.date.set(date);
+    }
+
+    public static StringProperty dateProperty() {
+        return date;
+    }
+
+    public static Branch getBranch() {
+        return branch.get();
+    }
+
+    public static void setBranch(Branch branch) {
+        StockInMasterViewModel.branch.set(branch);
+    }
+
+    public static ObjectProperty<Branch> branchProperty() {
+        return branch;
+    }
+
+    public static String getNote() {
+        return note.get();
+    }
+
+    public static void setNote(String note) {
+        StockInMasterViewModel.note.set(note);
+    }
+
+    public static StringProperty noteProperty() {
+        return note;
+    }
+
+    public static String getStatus() {
+        return status.get();
+    }
+
+    public static void setStatus(String status) {
+        StockInMasterViewModel.status.set(status);
+    }
+
+    public static StringProperty statusProperty() {
+        return status;
+    }
+
+    public static double getTotalCost() {
+        return Double.parseDouble(!totalCost.get().isEmpty() ? totalCost.get() : "0");
+    }
+
+    public static void setTotalCost(String totalCost) {
+        StockInMasterViewModel.totalCost.set(totalCost);
+    }
+
+    public static StringProperty totalCostProperty() {
+        return totalCost;
+    }
+
+    public static void resetProperties() {
+        setId(0);
+        setDate("");
+        setBranch(null);
+        setNote("");
+        setStatus("");
+        setTotalCost("");
+    }
+
+    public static void saveStockInMaster() {
+        StockInMaster stockInMaster = new StockInMaster(getDate(),
+                getBranch(),
+                getStatus(),
+                getNote());
+        stockInMaster.setStockInDetails(StockInDetailViewModel.stockInDetailsTempList);
+        StockInMasterDao.saveStockInMaster(stockInMaster);
+        resetProperties();
+        StockInDetailViewModel.stockInDetailsTempList.clear();
+        getStockInMasters();
+    }
+
+    public static ObservableList<StockInMaster> getStockInMasters() {
+        stockInMasterList.clear();
+        stockInMasterList.addAll(StockInMasterDao.fetchStockInMasters());
+        return stockInMasterList;
+    }
+
+}
