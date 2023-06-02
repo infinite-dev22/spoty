2edfee10d0f30aeb0449a5ce9fcf1889cf68b1d4
infinite-dev22 <mwigojm@gmail.com>
2023-06-02 08:42:17 +0300

    Create StockInMaster

---
+++ src/main/java/org/infinite/spoty/database/models/StockInMaster.java
@@ -0,0 +1,222 @@
+package org.infinite.spoty.database.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class StockInMaster implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @ManyToOne
+    private User user;
+    private String ref;
+    private Date date;
+    @ManyToOne
+    private Branch branch;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "stockIn")
+    private List<StockInDetail> stockInDetails;
+    private String shipping;
+    private double totalCost;
+    private String status;
+    private User approvedBy;
+    private User recordedBy;
+    private Date approvalDate;
+    private Date recordDate;
+    private String notes;
+    @Column(name = "created_at")
+    private Date createdAt;
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "updated_at")
+    private Date updatedAt;
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public StockInMaster() {
+    }
+
+    public StockInMaster(Date date,
+                         Branch branch,
+                         String status,
+                         String notes) {
+        this.date = date;
+        this.branch = branch;
+        this.status = status;
+        this.notes = notes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public String getBranchName() {
+        return (branch != null) ? branch.getName() : null;
+    }
+
+    public String getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(String shipping) {
+        this.shipping = shipping;
+    }
+
+    public double getTotal() {
+        return totalCost;
+    }
+
+    public void setTotal(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<StockInDetail> getStockInDetails() {
+        return stockInDetails;
+    }
+
+    public void setStockInDetails(List<StockInDetail> stockInDetails) {
+        this.stockInDetails = stockInDetails;
+    }
+
+    public User getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(User approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+
+    public User getReceivedBy() {
+        return recordedBy;
+    }
+
+    public void setReceivedBy(User recordedBy) {
+        this.recordedBy = recordedBy;
+    }
+
+    public Date getApprovalDate() {
+        return approvalDate;
+    }
+
+    public void setApprovalDate(Date approvalDate) {
+        this.approvalDate = approvalDate;
+    }
+
+    public Date getReceiveDate() {
+        return recordDate;
+    }
+
+    public void setReceiveDate(Date recordDate) {
+        this.recordDate = recordDate;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public User getRecordedBy() {
+        return recordedBy;
+    }
+
+    public void setRecordedBy(User recordedBy) {
+        this.recordedBy = recordedBy;
+    }
+
+    public Date getRecordDate() {
+        return recordDate;
+    }
+
+    public void setRecordDate(Date recordDate) {
+        this.recordDate = recordDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
