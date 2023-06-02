332417b558383d5b935b0f7fe9bfca4ae89c32f7
infinite-dev22 <mwigojm@gmail.com>
2023-06-02 08:43:02 +0300

    Create StockInDetail

---
+++ src/main/java/org/infinite/spoty/database/models/StockInDetail.java
@@ -0,0 +1,136 @@
+package org.infinite.spoty.database.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class StockInDetail implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "stockIn_id")
+    private StockInMaster stockIn;
+    @ManyToOne
+    private ProductDetail product;
+    private int quantity;
+    private String serialNo;
+    private String description;
+    private String location;
+    @Column(name = "created_at")
+    private Date createdAt;
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "updated_at")
+    private Date updatedAt;
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public StockInDetail() {
+    }
+
+    public StockInDetail(ProductDetail product,
+                         int quantity,
+                         String serialNo,
+                         String description,
+                         String location) {
+        this.product = product;
+        this.quantity = quantity;
+        this.serialNo = serialNo;
+        this.description = description;
+        this.location = location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public StockInMaster getStockIn() {
+        return stockIn;
+    }
+
+    public void setStockIn(StockInMaster stockIn) {
+        this.stockIn = stockIn;
+    }
+
+    public ProductDetail getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductDetail product) {
+        this.product = product;
+    }
+
+    public String getProductDetailName() {
+        return (product != null) ? product.getProduct().getName() + " " + product.getSaleUnit() : null;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
