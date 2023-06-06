4d838f136a0aa58844125a980cdc604aa2e43378
infinite-dev22 <mwigojm@gmail.com>
2023-06-06 03:36:03 +0300

    Add form input event listeners. Add logic to save or update Expense. Change validation.

--- src/main/java/org/infinite/spoty/forms/SupplierFormController.java
+++ src/main/java/org/infinite/spoty/forms/SupplierFormController.java
@@ -8,7 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
-import org.infinite.spoty.viewModels.SupplierVewModel;
+import javafx.util.converter.NumberStringConverter;
+import org.infinite.spoty.viewModels.SupplierViewModel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +17,7 @@ import java.util.ResourceBundle;
 import static org.infinite.spoty.GlobalActions.closeDialog;
 
 public class SupplierFormController implements Initializable {
+    public MFXTextField supplierID = new MFXTextField();
     @FXML
     public MFXFilledButton supplierFormSaveBtn;
     @FXML
@@ -39,22 +41,15 @@ public class SupplierFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        dialogOnActions();
-        supplierFormName.textProperty().addListener((observable, oldValue, newValue) -> supplierFormName.setTrailingIcon(null));
-//        supplierFormEmail.textProperty().addListener((observable, oldValue, newValue) -> supplierFormEmail.setTrailingIcon(null));
-//        supplierFormPhone.textProperty().addListener((observable, oldValue, newValue) -> supplierFormPhone.setTrailingIcon(null));
-//        supplierFormTown.textProperty().addListener((observable, oldValue, newValue) -> supplierFormTown.setTrailingIcon(null));
-//        supplierFormCity.textProperty().addListener((observable, oldValue, newValue) -> supplierFormCity.setTrailingIcon(null));
-//        supplierFormTaxNumber.textProperty().addListener((observable, oldValue, newValue) -> supplierFormTaxNumber.setTrailingIcon(null));
-//        supplierFormAddress.textProperty().addListener((observable, oldValue, newValue) -> supplierFormAddress.setTrailingIcon(null));
-
-        supplierFormName.textProperty().bindBidirectional(SupplierVewModel.nameProperty());
-        supplierFormEmail.textProperty().bindBidirectional(SupplierVewModel.emailProperty());
-        supplierFormPhone.textProperty().bindBidirectional(SupplierVewModel.phoneProperty());
-        supplierFormCity.textProperty().bindBidirectional(SupplierVewModel.cityProperty());
-        supplierFormCountry.textProperty().bindBidirectional(SupplierVewModel.countryProperty());
-        supplierFormTaxNumber.textProperty().bindBidirectional(SupplierVewModel.taxNumberProperty());
-        supplierFormAddress.textProperty().bindBidirectional(SupplierVewModel.addressProperty());
+        // Form input binding.
+        supplierID.textProperty().bindBidirectional(SupplierViewModel.idProperty(), new NumberStringConverter());
+        supplierFormName.textProperty().bindBidirectional(SupplierViewModel.nameProperty());
+        supplierFormEmail.textProperty().bindBidirectional(SupplierViewModel.emailProperty());
+        supplierFormPhone.textProperty().bindBidirectional(SupplierViewModel.phoneProperty());
+        supplierFormCity.textProperty().bindBidirectional(SupplierViewModel.cityProperty());
+        supplierFormCountry.textProperty().bindBidirectional(SupplierViewModel.countryProperty());
+        supplierFormTaxNumber.textProperty().bindBidirectional(SupplierViewModel.taxNumberProperty());
+        supplierFormAddress.textProperty().bindBidirectional(SupplierViewModel.addressProperty());
 
         dialogOnActions();
     }
@@ -62,49 +57,21 @@ public class SupplierFormController implements Initializable {
     private void dialogOnActions() {
         supplierFormCancelBtn.setOnAction((e) -> {
             closeDialog(e);
-            SupplierVewModel.resetProperties();
-
-            supplierFormName.setTrailingIcon(null);
-            supplierFormEmail.setTrailingIcon(null);
-            supplierFormPhone.setTrailingIcon(null);
-            supplierFormCity.setTrailingIcon(null);
-            supplierFormCountry.setTrailingIcon(null);
-            supplierFormTaxNumber.setTrailingIcon(null);
-            supplierFormAddress.setTrailingIcon(null);
+            SupplierViewModel.resetProperties();
+            supplierFormName.setStyle("-fx-border-color: red;");
         });
         supplierFormSaveBtn.setOnAction((e) -> {
             MFXIconWrapper icon = new MFXIconWrapper("fas-circle-exclamation", 20, Color.RED, 20);
 
             if (supplierFormName.getText().length() == 0) {
-                supplierFormName.setTrailingIcon(icon);
+                supplierFormName.setStyle("-fx-border-color: red;");
             }
-            if (supplierFormEmail.getText().length() == 0) {
-                supplierFormEmail.setTrailingIcon(icon);
-            }
-            if (supplierFormPhone.getText().length() == 0) {
-                supplierFormPhone.setTrailingIcon(icon);
-            }
-            if (supplierFormCity.getText().length() == 0) {
-                supplierFormCity.setTrailingIcon(icon);
-            }
-            if (supplierFormCountry.getText().length() == 0) {
-                supplierFormCountry.setTrailingIcon(icon);
-            }
-            if (supplierFormTaxNumber.getText().length() == 0) {
-                supplierFormTaxNumber.setTrailingIcon(icon);
-            }
-            if (supplierFormAddress.getText().length() == 0) {
-                supplierFormAddress.setTrailingIcon(icon);
-            }
-            if (supplierFormName.getText().length() > 0
-                    && supplierFormEmail.getText().length() > 0
-                    && supplierFormPhone.getText().length() > 0
-                    && supplierFormCity.getText().length() > 0
-                    && supplierFormCountry.getText().length() > 0
-                    && supplierFormTaxNumber.getText().length() > 0
-                    && supplierFormAddress.getText().length() > 0) {
-                SupplierVewModel.saveSupplier();
-                SupplierVewModel.resetProperties();
+            if (supplierFormName.getText().length() > 0) {
+                if (Integer.parseInt(supplierID.getText()) > 0)
+                    SupplierViewModel.updateItem(Integer.parseInt(supplierID.getText()));
+                else
+                    SupplierViewModel.saveSupplier();
+                SupplierViewModel.resetProperties();
                 closeDialog(e);
             }
         });
