22600761b7499e166858af998b0b20d53da0a1ab
infinite-dev22 <mwigojm@gmail.com>
2023-06-10 05:24:38 +0300

    Change transferMasterProductsTable to transferDetailTable. Add context menu to TransferMasterFormController.transferDetailTable. Add logic to save or update Transfers.

--- src/main/java/org/infinite/spoty/forms/TransferMasterFormController.java
+++ src/main/java/org/infinite/spoty/forms/TransferMasterFormController.java
@@ -7,11 +7,13 @@ import io.github.palexdev.materialfx.filter.IntegerFilter;
 import io.github.palexdev.materialfx.filter.StringFilter;
 import io.github.palexdev.mfxcomponents.controls.buttons.MFXElevatedButton;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -19,6 +21,8 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.StringConverter;
+import javafx.util.converter.NumberStringConverter;
+import org.infinite.spoty.database.dao.TransferDetailDao;
 import org.infinite.spoty.database.models.Branch;
 import org.infinite.spoty.database.models.TransferDetail;
 import org.infinite.spoty.viewModels.BranchViewModel;
@@ -32,14 +36,17 @@ import java.util.ResourceBundle;
 
 import static org.infinite.spoty.SpotResourceLoader.fxmlLoader;
 
+@SuppressWarnings("unchecked")
 public class TransferMasterFormController implements Initializable {
+    public MFXTextField transferDetailID = new MFXTextField();
+    public MFXTextField transferMasterID = new MFXTextField();
     @FXML
     public MFXFilterComboBox<Branch> transferMasterFromBranchId;
     public MFXFilterComboBox<Branch> transferMasterToBranchId;
     @FXML
     public MFXDatePicker transferMasterDate;
     @FXML
-    public MFXTableView<TransferDetail> transferMasterProductsTable;
+    public MFXTableView<TransferDetail> transferDetailTable;
     @FXML
     public MFXTextField transferMasterNote;
     @FXML
@@ -62,10 +69,12 @@ public class TransferMasterFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Input listeners.
         transferMasterFromBranchId.textProperty().addListener((observable, oldValue, newValue) -> transferMasterFromBranchId.setTrailingIcon(null));
         transferMasterToBranchId.textProperty().addListener((observable, oldValue, newValue) -> transferMasterToBranchId.setTrailingIcon(null));
         transferMasterDate.textProperty().addListener((observable, oldValue, newValue) -> transferMasterDate.setTrailingIcon(null));
-
+        // Input binding.
+        transferMasterID.textProperty().bindBidirectional(TransferMasterViewModel.idProperty(), new NumberStringConverter());
         transferMasterFromBranchId.valueProperty().bindBidirectional(TransferMasterViewModel.fromBranchProperty());
         transferMasterFromBranchId.setItems(BranchViewModel.branchesList);
         transferMasterFromBranchId.setConverter(new StringConverter<>() {
@@ -100,7 +109,6 @@ public class TransferMasterFormController implements Initializable {
         });
         transferMasterDate.textProperty().bindBidirectional(TransferMasterViewModel.dateProperty());
         transferMasterNote.textProperty().bindBidirectional(TransferMasterViewModel.noteProperty());
-
         transferMasterAddProductBtnClicked();
         Platform.runLater(this::setupTable);
     }
@@ -114,24 +122,64 @@ public class TransferMasterFormController implements Initializable {
         productQuantity.setRowCellFactory(product -> new MFXTableRowCell<>(TransferDetail::getQuantity));
 //        transferMasterType.setRowCellFactory(product -> new MFXTableRowCell<>(TransferDetail::getTransferType));
 
-        productName.prefWidthProperty().bind(transferMasterProductsTable.widthProperty().multiply(.4));
-        productQuantity.prefWidthProperty().bind(transferMasterProductsTable.widthProperty().multiply(.4));
+        productName.prefWidthProperty().bind(transferDetailTable.widthProperty().multiply(.4));
+        productQuantity.prefWidthProperty().bind(transferDetailTable.widthProperty().multiply(.4));
 //        transferMasterType.prefWidthProperty().bind(transferMasterProductsTable.widthProperty().multiply(.4));
 
-        transferMasterProductsTable.getTableColumns().addAll(productName, productQuantity); // , transferMasterType);
-        transferMasterProductsTable.getFilters().addAll(
+        transferDetailTable.getTableColumns().addAll(productName, productQuantity); // , transferMasterType);
+        transferDetailTable.getFilters().addAll(
                 new StringFilter<>("Name", TransferDetail::getProductDetailName),
                 new IntegerFilter<>("Quantity", TransferDetail::getQuantity)
 //                new StringFilter<>("Category", TransferDetail::getTransferType)
         );
         getTransferDetailTable();
-        transferMasterProductsTable.setItems(TransferDetailViewModel.transferDetailsTempList);
+        transferDetailTable.setItems(TransferDetailViewModel.transferDetailsTempList);
     }
 
     private void getTransferDetailTable() {
-        transferMasterProductsTable.setPrefSize(1000, 1000);
-        transferMasterProductsTable.features().enableBounceEffect();
-        transferMasterProductsTable.features().enableSmoothScrolling(0.5);
+        transferDetailTable.setPrefSize(1000, 1000);
+        transferDetailTable.features().enableBounceEffect();
+        transferDetailTable.features().enableSmoothScrolling(0.5);
+
+        transferDetailTable.setTableRowFactory(t -> {
+            MFXTableRow<TransferDetail> row = new MFXTableRow<>(transferDetailTable, t);
+            EventHandler<ContextMenuEvent> eventHandler = event -> {
+                showContextMenu((MFXTableRow<TransferDetail>) event.getSource()).show(transferDetailTable.getParent(), event.getScreenX(), event.getScreenY());
+                event.consume();
+            };
+            row.setOnContextMenuRequested(eventHandler);
+            return row;
+        });
+    }
+
+    private MFXContextMenu showContextMenu(MFXTableRow<TransferDetail> obj) {
+        MFXContextMenu contextMenu = new MFXContextMenu(transferDetailTable);
+        MFXContextMenuItem delete = new MFXContextMenuItem("Delete");
+        MFXContextMenuItem edit = new MFXContextMenuItem("Edit");
+
+        // Actions
+        // Delete
+        delete.setOnAction(e -> {
+            TransferDetailViewModel.getItem(obj.getData(), TransferDetailViewModel.transferDetailsTempList.indexOf(obj.getData()));
+            try {
+                if (Integer.parseInt(transferDetailID.getText()) > 0)
+                    TransferDetailDao.deleteTransferDetail(Integer.parseInt(transferDetailID.getText()));
+            } catch (NumberFormatException ignored) {
+                TransferDetailViewModel.removeTransferDetail(TransferDetailViewModel.transferDetailsTempList.indexOf(obj.getData()));
+            }
+            TransferDetailViewModel.getTransferDetails();
+            e.consume();
+        });
+        // Edit
+        edit.setOnAction(e -> {
+            TransferDetailViewModel.getItem(obj.getData(), TransferDetailViewModel.transferDetailsTempList.indexOf(obj.getData()));
+            dialog.showAndWait();
+            e.consume();
+        });
+
+        contextMenu.addItems(edit, delete);
+
+        return contextMenu;
     }
 
     private void transferMasterAddProductBtnClicked() {
@@ -159,15 +207,19 @@ public class TransferMasterFormController implements Initializable {
         if (transferMasterDate.getText().length() == 0) {
             transferMasterDate.setTrailingIcon(icon);
         }
-        if (transferMasterProductsTable.getTableColumns().isEmpty()) {
+        if (transferDetailTable.getTableColumns().isEmpty()) {
             // Notify table can't be empty
             System.out.println("Table can't be empty");
         }
         if (transferMasterFromBranchId.getText().length() > 0
                 && transferMasterToBranchId.getText().length() > 0
                 && transferMasterDate.getText().length() > 0
-                && !transferMasterProductsTable.getTableColumns().isEmpty()) {
-            TransferMasterViewModel.saveTransferMaster();
+                && !transferDetailTable.getTableColumns().isEmpty()) {
+            if (Integer.parseInt(transferMasterID.getText()) > 0) {
+                TransferMasterViewModel.updateItem(Integer.parseInt(transferMasterID.getText()));
+                transferMasterCancelBtnClicked();
+            } else
+                TransferMasterViewModel.saveTransferMaster();
             TransferMasterViewModel.resetProperties();
             TransferDetailViewModel.transferDetailsTempList.clear();
         }
