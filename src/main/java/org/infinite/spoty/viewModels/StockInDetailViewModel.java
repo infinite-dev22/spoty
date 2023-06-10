3a2c8247883b294c0bbb46ac39cb4b3e579e5430
infinite-dev22 <mwigojm@gmail.com>
2023-06-10 09:12:16 +0300

    Add updateStockInDetail(), getItem() * 2, updateItem(), removeStockInDetail().

--- src/main/java/org/infinite/spoty/viewModels/StockInDetailViewModel.java
+++ src/main/java/org/infinite/spoty/viewModels/StockInDetailViewModel.java
@@ -1,8 +1,12 @@
 package org.infinite.spoty.viewModels;
 
-import javafx.beans.property.*;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import org.infinite.spoty.database.dao.StocKInDetailDao;
 import org.infinite.spoty.database.models.ProductDetail;
 import org.infinite.spoty.database.models.StockInDetail;
 import org.infinite.spoty.database.models.StockInMaster;
@@ -10,7 +14,7 @@ import org.infinite.spoty.database.models.StockInMaster;
 public class StockInDetailViewModel {
     public static final ObservableList<StockInDetail> stockInDetailsList = FXCollections.observableArrayList();
     public static final ObservableList<StockInDetail> stockInDetailsTempList = FXCollections.observableArrayList();
-    private static final IntegerProperty id = new SimpleIntegerProperty();
+    private static final StringProperty id = new SimpleStringProperty();
     private static final ObjectProperty<ProductDetail> product = new SimpleObjectProperty<>();
     private static final ObjectProperty<StockInMaster> stockIn = new SimpleObjectProperty<>();
     private static final StringProperty quantity = new SimpleStringProperty("");
@@ -18,15 +22,15 @@ public class StockInDetailViewModel {
     private static final StringProperty description = new SimpleStringProperty("");
     private static final StringProperty location = new SimpleStringProperty("");
 
-    public static int getId() {
+    public static String getId() {
         return id.get();
     }
 
-    public static void setId(int id) {
+    public static void setId(String id) {
         StockInDetailViewModel.id.set(id);
     }
 
-    public static IntegerProperty idProperty() {
+    public static StringProperty idProperty() {
         return id;
     }
 
@@ -103,7 +107,7 @@ public class StockInDetailViewModel {
     }
 
     public static void resetProperties() {
-        setId(0);
+        setId("");
         setProduct(null);
         setQuantity("");
         setSerial("");
@@ -125,5 +129,50 @@ public class StockInDetailViewModel {
         stockInDetailsList.clear();
         stockInDetailsList.addAll(StockInDetailViewModel.getStockInDetails());
         return stockInDetailsList;
+    }
+
+    public static void updateStockInDetail(int index) {
+        StockInDetail stockInDetail = new StockInDetail(getProduct(),
+                getQuantity(),
+                getSerial(),
+                getDescription(),
+                getLocation());
+        stockInDetailsTempList.remove(index);
+        stockInDetailsTempList.add(index, stockInDetail);
+        resetProperties();
+    }
+
+    public static void getItem(int stockInDetailID) {
+        StockInDetail stockInDetail = StocKInDetailDao.findStockInDetail(stockInDetailID);
+        setId(String.valueOf(stockInDetail.getId()));
+        setProduct(stockInDetail.getProduct());
+        setQuantity(String.valueOf(stockInDetail.getQuantity()));
+        setSerial(stockInDetail.getSerialNo());
+        setDescription(stockInDetail.getDescription());
+        setLocation(stockInDetail.getLocation());
+        getStockInDetails();
+    }
+
+    public static void getItem(StockInDetail stockInDetail, int index) {
+        setId("index:" + index + ";");
+        setProduct(stockInDetail.getProduct());
+        setQuantity(String.valueOf(stockInDetail.getQuantity()));
+        setSerial(stockInDetail.getSerialNo());
+        setDescription(stockInDetail.getDescription());
+        setLocation(stockInDetail.getLocation());
+    }
+
+    public static void updateItem(int stockInDetailID) {
+        StockInDetail stockInDetail = new StockInDetail(getProduct(),
+                getQuantity(),
+                getSerial(),
+                getDescription(),
+                getLocation());
+        StocKInDetailDao.updateStockInDetail(stockInDetail, stockInDetailID);
+        getStockInDetails();
+    }
+
+    public static void removeStockInDetail(int index) {
+        stockInDetailsTempList.remove(index);
     }
 }
