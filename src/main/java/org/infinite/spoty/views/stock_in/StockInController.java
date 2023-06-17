c363d28f1b163213310f2c73b33a5bb3fb5d8b28
infinite-dev22 <mwigojm@gmail.com>
2023-06-17 05:57:23 +0300

    Fix Stock In master form oversize display.

--- src/main/java/org/infinite/spoty/views/stock_in/StockInController.java
+++ src/main/java/org/infinite/spoty/views/stock_in/StockInController.java
@@ -24,7 +24,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.input.ContextMenuEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
