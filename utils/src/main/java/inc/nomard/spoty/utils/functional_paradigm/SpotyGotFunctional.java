60c79c11a8f41f432b2dfb0f2353e22fb633bd46
infinite-dev22 <mwigojm@email.com>
2024-06-09 11:23:15 +0300

    fix POS UI add new functional interface "EventConsumer" to cater for functionals with ActionEvent parameter

--- utils/src/main/java/inc/nomard/spoty/utils/functional_paradigm/SpotyGotFunctional.java
+++ utils/src/main/java/inc/nomard/spoty/utils/functional_paradigm/SpotyGotFunctional.java
@@ -1,9 +1,16 @@
 package inc.nomard.spoty.utils.functional_paradigm;
 
+import javafx.event.*;
+
 public class SpotyGotFunctional {
     @FunctionalInterface
     public static interface ParameterlessConsumer {
         void run();
+    }
+
+    @FunctionalInterface
+    public static interface EventConsumer {
+        void run(ActionEvent event);
     }
 
     @FunctionalInterface
