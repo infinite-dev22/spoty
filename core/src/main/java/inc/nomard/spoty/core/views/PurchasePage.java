a46caf338a811f338f787791799be0cdc49107d3
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-06-30 14:01:49 +0300

    convert Purchase to use Java Code for views

--- core/src/main/java/inc/nomard/spoty/core/views/PurchasePage.java
+++ core/src/main/java/inc/nomard/spoty/core/views/PurchasePage.java
@@ -22,20 +22,23 @@ import inc.nomard.spoty.core.components.navigation.*;
 import inc.nomard.spoty.core.viewModels.purchases.*;
 import inc.nomard.spoty.core.views.components.*;
 import inc.nomard.spoty.core.views.previews.*;
+import inc.nomard.spoty.core.views.util.*;
 import inc.nomard.spoty.network_bridge.dtos.purchases.*;
 import io.github.palexdev.materialfx.controls.*;
 import io.github.palexdev.materialfx.controls.cell.*;
 import io.github.palexdev.materialfx.dialogs.*;
 import io.github.palexdev.materialfx.enums.*;
 import io.github.palexdev.materialfx.filter.*;
+import io.github.palexdev.mfxcomponents.controls.buttons.MFXButton;
+import io.github.palexdev.mfxcomponents.theming.enums.*;
 import io.github.palexdev.mfxresources.fonts.*;
 import java.io.*;
-import java.net.*;
 import java.util.*;
 import javafx.application.*;
 import javafx.collections.*;
 import javafx.event.*;
 import javafx.fxml.*;
+import javafx.geometry.*;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
@@ -44,27 +47,16 @@ import lombok.extern.java.*;
 
 @SuppressWarnings("unchecked")
 @Log
-public class PurchasesController implements Initializable {
-    private static PurchasesController instance;
+public class PurchasePage extends OutlinePage {
     private final Stage stage;
-    @FXML
-    public MFXTextField searchBar;
-    @FXML
-    public HBox actionsPane;
-    @FXML
-    public BorderPane contentPane;
-    @FXML
-    public MFXButton createBtn;
-    @FXML
-    public MFXProgressSpinner progress;
-    @FXML
-    public HBox leftHeaderPane;
-    @FXML
+    private MFXTextField searchBar;
     private MFXTableView<PurchaseMaster> masterTable;
+    private MFXProgressSpinner progress;
+    private MFXButton createBtn;
     private FXMLLoader viewFxmlLoader;
     private MFXStageDialog viewDialog;
 
-    public PurchasesController(Stage stage) {
+    public PurchasePage(Stage stage) {
         this.stage = stage;
         Platform.runLater(() ->
         {
@@ -74,18 +66,73 @@ public class PurchasesController implements Initializable {
                 throw new RuntimeException(e);
             }
         });
+        addNode(init());
     }
 
-    public static PurchasesController getInstance(Stage stage) {
-        if (instance == null) instance = new PurchasesController(stage);
-        return instance;
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public BorderPane init() {
+        var pane = new BorderPane();
+        pane.setTop(buildTop());
+        pane.setCenter(buildCenter());
         setIcons();
         setSearchBar();
-        Platform.runLater(this::setupTable);
+        setupTable();
+        createBtnAction();
+        return pane;
+    }
+
+    private HBox buildLeftTop() {
+        progress = new MFXProgressSpinner();
+        progress.setMinSize(30d, 30d);
+        progress.setPrefSize(30d, 30d);
+        progress.setMaxSize(30d, 30d);
+        progress.setVisible(false);
+        var hbox = new HBox(progress);
+        hbox.setAlignment(Pos.CENTER_LEFT);
+        hbox.setPadding(new Insets(0d, 10d, 0d, 10d));
+        HBox.setHgrow(hbox, Priority.ALWAYS);
+        return hbox;
+    }
+
+    private HBox buildCenterTop() {
+        searchBar = new MFXTextField();
+        searchBar.setPromptText("Search accounts");
+        searchBar.setFloatMode(FloatMode.DISABLED);
+        searchBar.setMinWidth(300d);
+        searchBar.setPrefWidth(500d);
+        searchBar.setMaxWidth(700d);
+        var hbox = new HBox(searchBar);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setPadding(new Insets(0d, 10d, 0d, 10d));
+        HBox.setHgrow(hbox, Priority.ALWAYS);
+        return hbox;
+    }
+
+    private HBox buildRightTop() {
+        createBtn = new MFXButton("Create");
+        createBtn.setVariants(ButtonVariants.FILLED);
+        var hbox = new HBox(createBtn);
+        hbox.setAlignment(Pos.CENTER_RIGHT);
+        hbox.setPadding(new Insets(0d, 10d, 0d, 10d));
+        HBox.setHgrow(hbox, Priority.ALWAYS);
+        return hbox;
+    }
+
+    private HBox buildTop() {
+        var hbox = new HBox();
+        hbox.getStyleClass().add("card-flat");
+        BorderPane.setAlignment(hbox, Pos.CENTER);
+        hbox.setPadding(new Insets(5d));
+        hbox.getChildren().addAll(buildLeftTop(), buildCenterTop(), buildRightTop());
+        return hbox;
+    }
+
+    private AnchorPane buildCenter() {
+        masterTable = new MFXTableView<>();
+        AnchorPane.setBottomAnchor(masterTable, 0d);
+        AnchorPane.setLeftAnchor(masterTable, 0d);
+        AnchorPane.setRightAnchor(masterTable, 0d);
+        AnchorPane.setTopAnchor(masterTable, 10d);
+        return new AnchorPane(masterTable);
     }
 
     private void setupTable() {
@@ -208,11 +255,11 @@ public class PurchasesController implements Initializable {
         delete.setOnAction(event -> new DeleteConfirmationDialog(() -> {
             PurchaseMasterViewModel.deleteItem(obj.getData().getId(), this::onSuccess, this::successMessage, this::errorMessage);
             event.consume();
-        }, obj.getData().getSupplierName() + "'s purchase", stage, contentPane));
+        }, obj.getData().getSupplierName() + "'s purchase", stage, this));
         // Edit
         edit.setOnAction(
                 e -> {
-                    Platform.runLater(() -> PurchaseMasterViewModel.getPurchaseMaster(obj.getData().getId(), this::createBtnClicked, this::errorMessage));
+                    Platform.runLater(() -> PurchaseMasterViewModel.getPurchaseMaster(obj.getData().getId(), this::createBtnAction, this::errorMessage));
                     e.consume();
                 });
         // View
@@ -227,8 +274,8 @@ public class PurchasesController implements Initializable {
         return contextMenu;
     }
 
-    public void createBtnClicked() {
-        BaseController.navigation.navigate(Pages.getPurchaseMasterFormPane());
+    public void createBtnAction() {
+        createBtn.setOnAction(event -> BaseController.navigation.navigate(Pages.getPurchaseMasterFormPane()));
     }
 
     private void onSuccess() {
@@ -251,7 +298,7 @@ public class PurchasesController implements Initializable {
                         .toStageDialogBuilder()
                         .initOwner(stage)
                         .initModality(Modality.WINDOW_MODAL)
-                        .setOwnerNode(contentPane)
+                        .setOwnerNode(this)
                         .setScrimPriority(ScrimPriority.WINDOW)
                         .setScrimOwner(true)
                         .setCenterInOwnerNode(false)
