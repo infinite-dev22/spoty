93f214c16b8b24424c9dfa4ce7eb43444584f8a3
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-06-30 14:28:32 +0300

    convert UnitOfMeasure to use Java Code for views

--- core/src/main/java/inc/nomard/spoty/core/views/UnitOfMeasurePage.java
+++ core/src/main/java/inc/nomard/spoty/core/views/UnitOfMeasurePage.java
@@ -21,20 +21,23 @@ import inc.nomard.spoty.core.components.message.enums.*;
 import inc.nomard.spoty.core.viewModels.*;
 import inc.nomard.spoty.core.views.components.*;
 import inc.nomard.spoty.core.views.forms.*;
+import inc.nomard.spoty.core.views.util.*;
 import inc.nomard.spoty.network_bridge.dtos.*;
 import io.github.palexdev.materialfx.controls.*;
 import io.github.palexdev.materialfx.controls.cell.*;
 import io.github.palexdev.materialfx.dialogs.*;
 import io.github.palexdev.materialfx.enums.*;
 import io.github.palexdev.materialfx.filter.*;
+import io.github.palexdev.mfxcomponents.controls.buttons.MFXButton;
+import io.github.palexdev.mfxcomponents.theming.enums.*;
 import io.github.palexdev.mfxresources.fonts.*;
 import java.io.*;
-import java.net.*;
 import java.util.*;
 import javafx.application.*;
 import javafx.collections.*;
 import javafx.event.*;
 import javafx.fxml.*;
+import javafx.geometry.*;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
@@ -43,26 +46,15 @@ import lombok.extern.java.*;
 
 @SuppressWarnings("unchecked")
 @Log
-public class UnitOfMeasureController implements Initializable {
-    private static UnitOfMeasureController instance;
+public class UnitOfMeasurePage extends OutlinePage {
     private final Stage stage;
-    @FXML
-    public MFXTableView<UnitOfMeasure> masterTable;
-    @FXML
-    public MFXTextField searchBar;
-    @FXML
-    public HBox actionsPane;
-    @FXML
-    public BorderPane contentPane;
-    @FXML
-    public MFXButton createBtn;
-    @FXML
-    public MFXProgressSpinner progress;
-    @FXML
-    public HBox leftHeaderPane;
+    private MFXTextField searchBar;
+    private MFXTableView<UnitOfMeasure> masterTable;
+    private MFXProgressSpinner progress;
+    private MFXButton createBtn;
     private MFXStageDialog dialog;
 
-    public UnitOfMeasureController(Stage stage) {
+    public UnitOfMeasurePage(Stage stage) {
         this.stage = stage;
         Platform.runLater(
                 () -> {
@@ -72,18 +64,73 @@ public class UnitOfMeasureController implements Initializable {
                         throw new RuntimeException(ex);
                     }
                 });
+        addNode(init());
     }
 
-    public static UnitOfMeasureController getInstance(Stage stage) {
-        if (instance == null) instance = new UnitOfMeasureController(stage);
-        return instance;
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public BorderPane init() {
+        var pane = new BorderPane();
+        pane.setTop(buildTop());
+        pane.setCenter(buildCenter());
         setIcons();
         setSearchBar();
-        Platform.runLater(this::setupTable);
+        setupTable();
+        createBtnAction();
+        return pane;
+    }
+
+    private HBox buildLeftTop() {
+        progress = new MFXProgressSpinner();
+        progress.setMinSize(30d, 30d);
+        progress.setPrefSize(30d, 30d);
+        progress.setMaxSize(30d, 30d);
+        progress.setVisible(false);
+        var hbox = new HBox(progress);
+        hbox.setAlignment(Pos.CENTER_LEFT);
+        hbox.setPadding(new Insets(0d, 10d, 0d, 10d));
+        HBox.setHgrow(hbox, Priority.ALWAYS);
+        return hbox;
+    }
+
+    private HBox buildCenterTop() {
+        searchBar = new MFXTextField();
+        searchBar.setPromptText("Search accounts");
+        searchBar.setFloatMode(FloatMode.DISABLED);
+        searchBar.setMinWidth(300d);
+        searchBar.setPrefWidth(500d);
+        searchBar.setMaxWidth(700d);
+        var hbox = new HBox(searchBar);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setPadding(new Insets(0d, 10d, 0d, 10d));
+        HBox.setHgrow(hbox, Priority.ALWAYS);
+        return hbox;
+    }
+
+    private HBox buildRightTop() {
+        createBtn = new MFXButton("Create");
+        createBtn.setVariants(ButtonVariants.FILLED);
+        var hbox = new HBox(createBtn);
+        hbox.setAlignment(Pos.CENTER_RIGHT);
+        hbox.setPadding(new Insets(0d, 10d, 0d, 10d));
+        HBox.setHgrow(hbox, Priority.ALWAYS);
+        return hbox;
+    }
+
+    private HBox buildTop() {
+        var hbox = new HBox();
+        hbox.getStyleClass().add("card-flat");
+        BorderPane.setAlignment(hbox, Pos.CENTER);
+        hbox.setPadding(new Insets(5d));
+        hbox.getChildren().addAll(buildLeftTop(), buildCenterTop(), buildRightTop());
+        return hbox;
+    }
+
+    private AnchorPane buildCenter() {
+        masterTable = new MFXTableView<>();
+        AnchorPane.setBottomAnchor(masterTable, 0d);
+        AnchorPane.setLeftAnchor(masterTable, 0d);
+        AnchorPane.setRightAnchor(masterTable, 0d);
+        AnchorPane.setTopAnchor(masterTable, 10d);
+        return new AnchorPane(masterTable);
     }
 
     private void setupTable() {
@@ -166,7 +213,7 @@ public class UnitOfMeasureController implements Initializable {
         delete.setOnAction(event -> new DeleteConfirmationDialog(() -> {
             UOMViewModel.deleteItem(obj.getData().getId(), this::onSuccess, this::successMessage, this::errorMessage);
             event.consume();
-        }, obj.getData().getName(), stage, contentPane));
+        }, obj.getData().getName(), stage, this));
         // Edit
         edit.setOnAction(
                 e -> {
@@ -192,7 +239,7 @@ public class UnitOfMeasureController implements Initializable {
                         .toStageDialogBuilder()
                         .initOwner(stage)
                         .initModality(Modality.WINDOW_MODAL)
-                        .setOwnerNode(contentPane)
+                        .setOwnerNode(this)
                         .setScrimPriority(ScrimPriority.WINDOW)
                         .setScrimOwner(true)
                         .get();
@@ -200,8 +247,8 @@ public class UnitOfMeasureController implements Initializable {
         io.github.palexdev.mfxcomponents.theming.MaterialThemes.PURPLE_LIGHT.applyOn(dialog.getScene());
     }
 
-    public void createBtnClicked() {
-        dialog.showAndWait();
+    public void createBtnAction() {
+        createBtn.setOnAction(event -> dialog.showAndWait());
     }
 
     private void onSuccess() {
