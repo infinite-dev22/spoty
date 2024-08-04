55f3162e5e3fb6d127529ab4f9fbd12bb184547e
infinite-dev22 <mwigojm@gmail.com>
2024-08-04 18:48:33 +0300

    Create app utils class

---
+++ utils/src/main/java/inc/nomard/spoty/utils/AppUtils.java
@@ -0,0 +1,9 @@
+package inc.nomard.spoty.utils;
+
+import java.text.*;
+
+public class AppUtils {
+    public static DecimalFormat decimalFormatter() {
+        return new DecimalFormat("###,###,###,###,###,###,###,###,###,###.##");
+    }
+}
