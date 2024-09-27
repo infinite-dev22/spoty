d0049ba42bbe6fcc9d92f62783ce953524f822ce
infinite-dev22 <mwigojm@gmail.com>
2024-09-28 02:16:41 +0300

    remove the stupid transparent windows - Shit really looked bogus

--- core/src/main/java/inc/nomard/spoty/core/views/layout/ApplicationWindowImpl.java
+++ core/src/main/java/inc/nomard/spoty/core/views/layout/ApplicationWindowImpl.java
@@ -13,9 +13,6 @@ import javafx.scene.paint.Color;
 
 public class ApplicationWindowImpl extends StackPane implements ApplicationWindow {
     protected final StackPane window1 = new StackPane();
-    protected final StackPane window2 = new StackPane();
-    protected final StackPane window3 = new StackPane();
-    protected final StackPane window4 = new StackPane();
     protected final GlassPane contentLayer = new GlassPane();
     protected boolean isRendered = false;
 
@@ -27,13 +24,13 @@ public class ApplicationWindowImpl extends StackPane implements ApplicationWindo
         AppManager.setParent(this);
         createPageLayout();
         AppManager.setMorphPane(contentLayer);
-        blur();
     }
 
     protected void createPageLayout() {
         setMinWidth(ApplicationWindow.MAX_WIDTH);
         setMinHeight(ApplicationWindow.MAX_HEIGHT);
-        getChildren().setAll(new StackPane(window1, window2, window3, window4), contentLayer);
+        window1.getStyleClass().add("windows");
+        getChildren().setAll(window1, contentLayer);
     }
 
     protected void addNode(Node node) {
@@ -80,37 +77,6 @@ public class ApplicationWindowImpl extends StackPane implements ApplicationWindo
 
         isRendered = true;
         onRendered();
-    }
-
-    public void blur() {
-        window1.setEffect(new GaussianBlur(60));
-        window1.setMinWidth(MAX_WIDTH);
-        window1.setMinHeight(MAX_HEIGHT);
-
-        window2.setEffect(new GaussianBlur(60));
-        window2.setMinWidth(MAX_WIDTH);
-        window2.setMinHeight(MAX_HEIGHT);
-        window2.getStyleClass().add("luminosity");
-
-        window3.setEffect(new GaussianBlur(60));
-        window3.setMinWidth(MAX_WIDTH);
-        window3.setMinHeight(MAX_HEIGHT);
-        window3.getStyleClass().add("tint");
-
-        window4.setEffect(new GaussianBlur(60));
-        window4.setMinWidth(MAX_WIDTH);
-        window4.setMinHeight(MAX_HEIGHT);
-        window4.getStyleClass().add("noise");
-//        window4.setBackground(
-//                new Background(
-//                        new BackgroundFill(Color.rgb(0, 0, 0, 0.6), CornerRadii.EMPTY, Insets.EMPTY)
-//                )
-//        );
-        this.setBackground(
-                new Background(
-                        new BackgroundFill(Color.rgb(0, 0, 0, 0), CornerRadii.EMPTY, Insets.EMPTY)
-                )
-        );
     }
 
     // Some properties can only be obtained after node placed
