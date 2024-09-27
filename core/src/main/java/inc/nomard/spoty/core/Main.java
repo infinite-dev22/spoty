00ea206e419b5bddee9939908f46d4dff365b5cd
infinite-dev22 <mwigojm@gmail.com>
2024-09-28 02:28:18 +0300

    Add main method for easy loading and launching(also coz the native image compile required it)

--- core/src/main/java/inc/nomard/spoty/core/Main.java
+++ core/src/main/java/inc/nomard/spoty/core/Main.java
@@ -113,6 +113,10 @@ public class Main extends Application {
         }
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void init() {
         PreloadedData.preloadImages();
