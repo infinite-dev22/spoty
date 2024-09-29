8c1e24fa68d9e67567e2c7ed0444879d1e0e23b7
infinite-dev22 <mwigojm@gmail.com>
2024-09-30 02:39:14 +0300

    Add string converter

---
+++ core/src/main/java/inc/nomard/spoty/core/views/util/FunctionalStringConverter.java
@@ -0,0 +1,98 @@
+package inc.nomard.spoty.core.views.util;
+
+import javafx.util.StringConverter;
+
+import java.util.function.Function;
+
+/**
+ * A functional alternative to {@link StringConverter}.
+ */
+@FunctionalInterface
+public interface FunctionalStringConverter<T> {
+
+	/**
+	 * Converts the given {@code String} to an object of type {@code T}
+	 */
+	T fromString(String s);
+
+	/**
+	 * Default implementation throws {@link UnsupportedOperationException}.
+	 */
+	default String toString(T t) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * @return a new {@link StringConverter} which uses the given function
+	 * to convert a String to an object of type T
+	 */
+	static <T> StringConverter<T> converter(Function<String, T> fsFunction) {
+		return new StringConverter<>() {
+			@Override
+			public String toString(T t) {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public T fromString(String string) {
+				return fsFunction.apply(string);
+			}
+		};
+	}
+
+	/**
+	 * @return a new {@link StringConverter} which uses the given functions
+	 * to convert a String to an object of type T and vice versa.
+	 */
+	static <T> StringConverter<T> converter(Function<String, T> fsFunction, Function<T, String> tsFunction) {
+		return new StringConverter<>() {
+			@Override
+			public String toString(T t) {
+				return t != null ? tsFunction.apply(t) : "";
+			}
+
+			@Override
+			public T fromString(String string) {
+				return fsFunction.apply(string);
+			}
+		};
+	}
+
+	/**
+	 * @return a new {@link StringConverter} which is only capable of converting a String
+	 * to an object of type T
+	 * @throws UnsupportedOperationException when using the toString(T) method
+	 */
+	static <T> StringConverter<T> from(Function<String, T> fsFunction) {
+		return new StringConverter<>() {
+			@Override
+			public String toString(T t) {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public T fromString(String string) {
+				return fsFunction.apply(string);
+			}
+		};
+	}
+
+	/**
+	 * @return a new {@link StringConverter} which is only capable of converting an object
+	 * of type T to a String
+	 * @throws UnsupportedOperationException when using the fromString(String) method
+	 */
+	static <T> StringConverter<T> to(Function<T, String> tsFunction) {
+		return new StringConverter<>() {
+			@Override
+			public String toString(T t) {
+				return tsFunction.apply(t);
+			}
+
+			@Override
+			public T fromString(String string) {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+}
