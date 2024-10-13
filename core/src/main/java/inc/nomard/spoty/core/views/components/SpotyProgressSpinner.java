351f0fdf23835046bc9101efab4f7107f3454200
infinite-dev22 <mwigojm@gmail.com>
2024-10-13 14:33:26 +0300

    Clean code

--- core/src/main/java/inc/nomard/spoty/core/views/components/SpotyProgressSpinner.java
+++ core/src/main/java/inc/nomard/spoty/core/views/components/SpotyProgressSpinner.java
@@ -1,25 +1,11 @@
 package inc.nomard.spoty.core.views.components;
 
 import io.github.palexdev.materialfx.controls.MFXProgressSpinner;
-import javafx.beans.NamedArg;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.ReadOnlyBooleanProperty;
-import javafx.beans.property.ReadOnlyBooleanWrapper;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.scene.Node;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.Skin;
-import javafx.util.StringConverter;
 
-/**
- * A ProgressIndicator that displays progress value as a ring that gradually
- * empties out as a task is completed.
- */
 public class SpotyProgressSpinner extends MFXProgressSpinner {
-    private final String STYLE_CLASS = "spoty-progress-spinner";
+
     public SpotyProgressSpinner() {
         super();
-
-        getStyleClass().add(STYLE_CLASS);
+        getStyleClass().add("spoty-progress-spinner");
     }
 }
